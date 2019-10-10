1af3b23cf900ef3df34dae4f40163850429ffdfd
marinelay <marinelay7@gmail.com>
2019-10-11 01:41:53 +0900

    Delete note

--- src/config.js
+++ src/config.js
@@ -1,16 +1,17 @@
 export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
-    REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
-    BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
+    REGION: "us-ease-2",
+    BUCKET: "notes-app-uploads-ws"
   },
   apiGateway: {
-    REGION: "YOUR_API_GATEWAY_REGION",
-    URL: "YOUR_API_GATEWAY_URL"
+    REGION: "us-east-2",
+    URL: "https://u57e955l12.execute-api.us-east-2.amazonaws.com/prod"
   },
   cognito: {
-    REGION: "YOUR_COGNITO_REGION",
-    USER_POOL_ID: "YOUR_COGNITO_USER_POOL_ID",
-    APP_CLIENT_ID: "YOUR_COGNITO_APP_CLIENT_ID",
-    IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_PBJzyHs7t",
+    APP_CLIENT_ID: "4q09bd8ta8c3a05240oe34vk82",
+    IDENTITY_POOL_ID: "us-east-2:9a1782ca-8758-49eb-a5fa-402598935d4f"
   }
 };
